687d5f1c7baf0482277a1e44008e93e438f6947e
teotripura-prog <teotripura@gmail.com>
2026-08-02 11:59:10 +0530

    firebase.js
    Set up Firebase SDK with authentication and Firestore.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,31 @@
 
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "tiprasa-smm-panel.firebaseapp.com",
+  projectId: "tiprasa-smm-panel",
+  storageBucket: "tiprasa-smm-panel.appspot.com",
+  messagingSenderId: "667620321290",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+console.log("Firebase Connected Successfully");
